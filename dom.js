b2838f41ce3b7aa5584a457f99d0ac06c2ba267c
Charanj2000 <charanjanardhan118@gmail.com>
2022-10-13 16:18:07 +0530

    getelementbytag

--- dom.js
+++ dom.js
@@ -27,13 +27,26 @@ document.getElementById("add").style.fontWeight = "bold"
 
 
 //GETELEMENTSBYCLASSNAME//
-var items =  document.getElementsByClassName('list-group-item');
+/* var items =  document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[1]);
 items[1].textContent = "hello 2 ";
 items[1].style.fontWeight='bold';
  items[1].style.backgroundColor='yellow'; 
  items[2].style.backgroundColor='green'; 
+ items[4].style.backgroundColor='red'; 
 for(var i=0 ; i< items.length ; i++){
     items[i].style.fontWeight='bold';
+} */
+
+//GETELEMENTSBYTAGNAME//
+var li =  document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = "hello 2 ";
+li[1].style.fontWeight='bold';
+li[1].style.backgroundColor='yellow'; 
+ 
+for(var i=0 ; i< li.length ; i++){
+    li[i].style.fontWeight='bold';
 }
