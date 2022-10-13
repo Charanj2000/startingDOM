b58f7674ec1609d95355e88e147f8f0b5e2f3eb0
Charanj2000 <charanjanardhan118@gmail.com>
2022-10-13 16:05:40 +0530

    getelementbyclass

--- dom.js
+++ dom.js
@@ -24,3 +24,16 @@ header.style.borderBottom ='dotted 3px #000';
 
 document.getElementById("add").style.color = "green"
 document.getElementById("add").style.fontWeight = "bold"
+
+
+//GETELEMENTSBYCLASSNAME//
+var items =  document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = "hello 2 ";
+items[1].style.fontWeight='bold';
+ items[1].style.backgroundColor='yellow'; 
+ items[2].style.backgroundColor='green'; 
+for(var i=0 ; i< items.length ; i++){
+    items[i].style.fontWeight='bold';
+}
